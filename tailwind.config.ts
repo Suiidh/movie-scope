5f6ab5a999108305a30b0c76cd89b38e87a72054
Enzo Marion <marionen@iut1.u-ga.fr>
2024-12-19 13:51:44 +0100

    Ajout de transition

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,11 +14,21 @@ const config: Config = {
       },
       animation: {
         "spin-slow": "spin 1s linear infinite",
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        wiggle: 'wiggle 0.3s ease-in-out',
       },
       keyframes: {
         spin: {
           "0%": { transform: "rotate(0deg)" },
           "100%": { transform: "rotate(360deg)" },
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
         },
       },
     },
